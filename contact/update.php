4fbb20dc459d9dfdd7ab9538702a467809c3ca48
MD. WASIFUR RAHMAN SIYAM <71380364+Wasifur-Rahman-Siyam@users.noreply.github.com>
2022-02-03 18:46:47 +0600

    adding condition in editi page

--- contact/update.php
+++ contact/update.php
@@ -5,6 +5,7 @@ $_id=$_POST['id'];
 $_name=$_POST['name'];
 $_email=$_POST['email'];
 $_subject=$_POST['subject'];
+$_toggle=$_POST['toggle'];
 
 $servername = "localhost";
 $username = "root";
@@ -14,13 +15,14 @@ $conn = new PDO("mysql:host=$servername;dbname=crud_pondit", $username, $passwor
   // set the PDO error mode to exception
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
- $query= "UPDATE `contact` SET `name` = :name , `email` = :email ,`subject` =:subject	WHERE `contact`.`id` = :id;";
+ $query= "UPDATE `contact` SET `name` = :name , `email` = :email ,`subject` =:subject, `toggle` = :toggle	WHERE `contact`.`id` = :id;";
 
  $stmt= $conn->prepare($query); 
  $stmt->bindParam(':id', $_id );
  $stmt->bindParam(':name', $_name );
  $stmt->bindParam(':email', $_email);
  $stmt->bindParam(':subject', $_subject);
+ $stmt->bindParam(':toggle', $_toggle);
 
  $result= $stmt->execute();
  if($result){
